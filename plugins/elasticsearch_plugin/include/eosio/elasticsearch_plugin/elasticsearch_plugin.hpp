c7fa86e54856751a25fc7a0125f239fd94cebcf4
wlbf <lbf1353@live.com>
2018-08-28 20:15:39 +0800

    modify Dockerfile 1.change Dockerfile to use specific branch 2.fix APPBASE_PLUGIN_REQUIRES 2.correct some log

--- plugins/elasticsearch_plugin/include/eosio/elasticsearch_plugin/elasticsearch_plugin.hpp
+++ plugins/elasticsearch_plugin/include/eosio/elasticsearch_plugin/elasticsearch_plugin.hpp
@@ -15,10 +15,10 @@ using namespace appbase;
  */
 class elasticsearch_plugin : public appbase::plugin<elasticsearch_plugin> {
 public:
+   APPBASE_PLUGIN_REQUIRES((chain_plugin))
    elasticsearch_plugin();
    virtual ~elasticsearch_plugin();
  
-   APPBASE_PLUGIN_REQUIRES()
    virtual void set_program_options(options_description&, options_description& cfg) override;
  
    void plugin_initialize(const variables_map& options);
