302d236c1f0ad630f575bbc68c34d5152d1a07b1
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-09-04 14:57:11 +0900

    refund undelegated tokens in 2 minutes

--- contracts/eosio.system/delegate_bandwidth.cpp
+++ contracts/eosio.system/delegate_bandwidth.cpp
@@ -28,7 +28,7 @@ namespace eosiosystem {
    using std::map;
    using std::pair;
 
-   static constexpr time refund_delay = 3*24*3600;
+   static constexpr time refund_delay = 2 * 60; // refund in 2 minutes
    static constexpr time refund_expiration_time = 3600;
 
    struct user_resources {
