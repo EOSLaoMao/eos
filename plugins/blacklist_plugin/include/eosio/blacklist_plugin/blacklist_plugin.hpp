383e3cae163c8833328ba7d0647ff369604b3855
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-09-15 16:10:21 +0900

    add http_plugin to APPBASE_PLUGIN_REQUIRES

--- plugins/blacklist_plugin/include/eosio/blacklist_plugin/blacklist_plugin.hpp
+++ plugins/blacklist_plugin/include/eosio/blacklist_plugin/blacklist_plugin.hpp
@@ -26,7 +26,7 @@ public:
    blacklist_plugin();
    virtual ~blacklist_plugin();
  
-   APPBASE_PLUGIN_REQUIRES((producer_plugin)(chain_plugin))
+   APPBASE_PLUGIN_REQUIRES((producer_plugin)(chain_plugin)(http_plugin))
    virtual void set_program_options(options_description&, options_description& cfg) override;
  
    void plugin_initialize(const variables_map& options);
