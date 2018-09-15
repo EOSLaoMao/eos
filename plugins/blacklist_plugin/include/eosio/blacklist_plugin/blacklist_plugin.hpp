f4014f37ad994a77f3c44cedb3aedf99c2016b54
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-09-15 12:53:21 +0900

    typo corrected

--- plugins/blacklist_plugin/include/eosio/blacklist_plugin/blacklist_plugin.hpp
+++ plugins/blacklist_plugin/include/eosio/blacklist_plugin/blacklist_plugin.hpp
@@ -12,10 +12,10 @@ using namespace appbase;
 /**
  *  This is a template plugin, intended to serve as a starting point for making new plugins
  */
-class producer_blacklist_plugin : public appbase::plugin<template_plugin> {
+class producer_blacklist_plugin : public appbase::plugin<producer_blacklist_plugin> {
 public:
-   template_plugin();
-   virtual ~template_plugin();
+   producer_blacklist_plugin();
+   virtual ~producer_blacklist_plugin();
  
    APPBASE_PLUGIN_REQUIRES((producer_plugin)(chain_plugin))
    virtual void set_program_options(options_description&, options_description& cfg) override;
@@ -25,7 +25,7 @@ public:
    void plugin_shutdown();
 
 private:
-   std::unique_ptr<class template_plugin_impl> my;
+   std::unique_ptr<class producer_blacklist_plugin_impl> my;
 };
 
 }
